26f7ade8d44094a31e2c22a0c373b13b7d555245
rezadulhaq <dhiaulhaqreza@gmail.com>
2022-12-09 07:25:50 +0700

    done

--- routes/user.js
+++ routes/user.js
@@ -12,13 +12,12 @@ app.use((req,res,next) => {
 
 app.get('/', Controller.home) // home
 app.get('/course', Controller.allCourse)
-// app.get('/category', Controller.categoryList) //to category page
-app.get('/category/:categoryId', Controller.courseList) //to courses list by category
-// app.get('/category/courses/:courseId/buy', Controller.buy) //for buying course
 app.get('/profile', Controller.profile) //to user profile page
+app.get('/category/:categoryId', Controller.courseList) //to courses list by category
 app.get('/profile/edit/:profileId', Controller.editProfileForm) // to form edit profile page
 app.post('/profile/edit/:profileId', Controller.editProfile) // update database user
 app.get('/profile/:courseId/delete', Controller.deleteCourse) //for delete course in profile page
 
 app.get('/:courseId/buy', Controller.buy)
+
 module.exports = app
